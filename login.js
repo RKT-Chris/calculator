0599a48d59534c464753c86f34a780ced82df710
Rakotohasy Chris <chrisrakotohasy@gmail.com>
2023-10-11 21:28:29 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,3 @@
+// $("#submit").click(function () {
+//     $("#txt").text("Hi, " + $("#first_name").val() + " " + $("#last name").val() + "now I know your mail I can do some thing wh=ith it")
+// })
---
+++ serious.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#animations").click(function () {
+        $("#animations_div").slideToggle(500)
+    });
+    $("#login").click(function () {
+        
+    })
+})
